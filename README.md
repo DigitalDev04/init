238986090f6aea3098c8245bb88e507de3dcfc5a
unknown <digital.dev04@gmail.com>
2024-08-30 09:31:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+- git init
+- git remote add origin https://github.com/antoine2702/init-project.git
+- git pull origin master
+- git remote remove origin
+
+Création du dossier database
+
+#commande for start  :
+- docker compose up -d
---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Docker</title>
+</head>
+
+<body>
+    <h1>Docker - Hello, World!</h1>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
